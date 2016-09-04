96f9777c141071167acbed3cb658b74294ea5800
M3nin0 <phelpsmemo@gmail.com>
2016-09-04 14:38:52 -0300

    Primeiros exercícios em Shell e adição do gitignore

---
+++ list-00/shell/exercise-00/exercise-00.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo Insira um valor: 
+read num1
+echo Insira mais um valor: 
+read num2
+
+echo $(($num1+$num2))
---
+++ list-00/shell/exercise-01/exercise-01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Metros em milimetros
+
+echo "Insira os valores em metros: "
+read meters
+
+echo "O valor convertido é $((meters * 1000)) milimetros"
